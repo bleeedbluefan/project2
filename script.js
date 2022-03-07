fbde6ed95c59063cd405b7ce79d1fd048f2846ff
Justin McKeen <justin_mckeen@yahoo.com>
2022-03-06 23:25:08 -0700

    Final push

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-document.getElementById("email-Submit").addEventListener("click", function(event) {
+var num = 0;
+document.getElementById("catButton").addEventListener("click", function(event) {
     event.preventDefault();
-    const value = document.getElementById("email-Input").value;
-    if (value === "")
-    return;
-    console.log(value);
-    
-    const url = "https://api.mailboxvalidator.com/v1/validation/single?email=" + value + "&key=9FBWDG72P5UFD579OH0U&format=json";
+    const url = "https://cat-fact.herokuapp.com/facts";
     fetch(url)
         .then(function(response){
             return response.json();
         }).then(function(json) {
             console.log(json);
-            
-
+            let results = [];
+            let text = "";
+            for (let i = 0; i < 5; i++) {
+                results[i] = json[i].text;
+            }
+            text +="<p class='cat-text'>" + json[num].text + "</p>";
+            num = num + 1;
+            if (num == 5) {
+                num = 0;
+            }
+            document.getElementById("catButton").innerHTML = text;
+            document.getElementById("catButton").style.backgroundImage="url(/images/cat" + num + ".jpeg)";
         });
 });
